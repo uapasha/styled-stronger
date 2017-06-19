96c960676f5b224ee78eb1fc49a6a6caa5e5c689
uapasha <uapasha@gmail.com>
2017-06-19 16:04:13 +0300

    bug fixes

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 import repeat from 'repeat-string';
 
 const enhanced = (literal, power) => {
+  const lastElement = literal.length - 1;
   const first = `${repeat('&', power)} { ${literal[0]}`;
-  const last = `${literal[literal.length] || ''} }`;
-  return [first, ...literal.slice(1, literal.length - 1), last];
+  const last = `${literal[lastElement] || ''} }`;
+  return [first, ...literal.slice(1, lastElement), last];
 };
 
 export const enhanceStyled = (fn, power = 1) => (literal, ...functions) =>
